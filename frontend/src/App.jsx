01d7ef86bd39aa880df7bae581ad4fb4cad56351
Madhu Maya Shrestha <z5453060@ad.unsw.edu.au>
2024-07-12 16:09:33 +1000

    Added routes + pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import StateMap from "./pages/StateMap";
+import SeasonalFruits from "./pages/SeasonalFruits";
+import Nutrients from "./pages/Nutrients";
+import Fruit from "./pages/Fruit";
+import Search from "./pages/Search";
+import Game from "./pages/Game";
+import Surprise from "./pages/Surprise";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/state-map" element={<StateMap />} />
+        <Route path="/seasonal-fruits/:state" element={<SeasonalFruits />} />
+        <Route path="/nutrients/:nutrientId" element={<Nutrients />} />
+        <Route path="/fruit/:fruitId" element={<Fruit />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/surprise" element={<Surprise />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
+export default App;
---
+++ frontend/src/pages/Surprise.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Surprise() {
+  return (
+    <div>
+      <h1>Surprise Page</h1>
+    </div>
+  );
+}
+
+export default Surprise;
