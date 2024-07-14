22007239aed2ebc70ffabf85ec9bea422201ec51
Brendan Ta <brendan.ta@student.unsw.edu.au>
2024-07-14 19:32:57 +1000

    Setup backend

---
+++ backend/src/routes/seasonal-fruits.ts
@@ -0,0 +1,10 @@
+interface Fruit {
+    name: String
+    // Enter rest of parameters
+}
+
+export function seasonalFruitsByState(state: String) : Array<Fruit> {
+    return [{
+        name: "Fill this in!"
+    }];
+}
--- frontend/src/pages/SeasonalFruits.jsx
+++ frontend/src/pages/SeasonalFruits.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios';
 import { useParams } from "react-router-dom";
 
 function SeasonalFruits() {
   const { state } = useParams();
+
+  // Example of fetching from backend ==> suggest that you put it in a separate file
+  useEffect(async () => {
+    try {
+      const response = await axios.get(`http://localhost:5180/seasonal-fruits?state=${state}`, {
+        method: 'GET',
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  }, [state]);
 
   return (
     <div>
