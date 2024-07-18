29e92835398c769a80367d7f7f45d2967894caf1
Brendan Ta <brendan.ta@student.unsw.edu.au>
2024-07-18 13:47:11 +1000

    Set up our App with MantineUI!

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import '@mantine/core/styles.css';
+
+import { MantineProvider, createTheme } from '@mantine/core';
+
+// NOTE: the website had this for default setup but not Vite setup so I included it here in case
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MantineProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </MantineProvider>
 )
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Button } from "@mantine/core";
 
 function Landing() {
   return (
     <div>
+      {/* Example Usage for Mantine Components */}
+      <Button variant="filled" size="xl" radius="md">Button</Button>
       <h1>Landing Page xD</h1>
       <p>Type /route into link to go the page u wanna go to :P</p>
       <a href="https://docs.google.com/document/d/1iFcgbbb_5nNqy9EMgA2oaZ2ZTpvSof95XA_n9mv16kI/edit">
