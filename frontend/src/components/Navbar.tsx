edc13371b85333658504b430c1137d260026bb4b
Madhu Maya Shrestha <z5453060@ad.unsw.edu.au>
2024-08-27 22:54:44 +1000

    adding navbar component

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,10 +1,71 @@
 import React from "react";
+import { useState } from "react";
+import { Center, Tooltip, UnstyledButton, Stack, rem } from "@mantine/core";
+import {
+  FaChevronRight,
+  FaLocationDot,
+  FaGamepad,
+  FaDice,
+} from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
+import { CgPill } from "react-icons/cg";
+import { GoHome } from "react-icons/go";
+import classes from "./Navbar.module.css";
+
+function NavbarLink({ icon: Icon, label, active, onClick }) {
+  return (
+    <Tooltip label={label} position="right" transitionProps={{ duration: 0 }}>
+      <UnstyledButton
+        onClick={onClick}
+        className={classes.link}
+        data-active={active || undefined}
+      >
+        <Icon style={{ width: rem(28), height: rem(28) }} />
+      </UnstyledButton>
+    </Tooltip>
+  );
+}
+
+const icondata = [
+  { icon: GoHome, label: "Home" },
+  { icon: FaSearch, label: "Search" },
+  { icon: FaLocationDot, label: "State Map" },
+  { icon: FaGamepad, label: "Game" },
+  { icon: FaDice, label: "Surprise" },
+  { icon: CgPill, label: "Vitamin" },
+];
 
 function Navbar() {
+  const [active, setActive] = useState(2);
+
+  const links = icondata.map((link, index) => (
+    <NavbarLink
+      {...link}
+      key={link.label}
+      active={index === active}
+      onClick={() => setActive(index)}
+    />
+  ));
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className={classes.navbar}>
+      <FaChevronRight
+        type="mark"
+        size={25}
+        style={{
+          display: "flex",
+          alignSelf: "center",
+          marginTop: "20px",
+          color: "#F3725E",
+        }}
+      />
+
+      <div className={classes.navbarMain}>
+        <Stack justify="center" gap={16}>
+          {links}
+        </Stack>
+      </div>
+    </nav>
   );
 }
 
---
+++ frontend/src/components/NavbarExpanded.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavbarExpanded() {
+  return (
+    <div>
+      <h1>Lalalalala</h1>
+    </div>
+  );
+}
+
+export default NavbarExpanded;
