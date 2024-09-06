0e236a76174c0f116536d189fd3aaaf10044d78a
Tet Nay Lin <tetnaylin168@gmail.com>
2024-09-06 22:15:17 +1000

    Add blur on hover and shadow to map

--- frontend/src/components/BasicMap.tsx
+++ frontend/src/components/BasicMap.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Image, Overlay, AspectRatio, MantineProvider } from '@mantine/core';
 import { useHover } from "@mantine/hooks";
-import "./BasicMap.module.css";
+// TODO: FIX - import still works tho so meh XD
+import classes from "./BasicMap.module.css";
 
 function BasicMap() {
   const { hovered, ref } = useHover();
   return (
-
-    // TODO: FIX
-    // <div className={background}>
-
     <AspectRatio maw={500} mx="auto" pos="relative" ref={ref}>
-      <Image 
+      <Image className={classes.box}
         radius="md"
         // h={200}
         h={"100%"}
@@ -21,9 +18,6 @@ function BasicMap() {
         />
       <Overlay color="#000" backgroundOpacity={hovered ? 0.02:0.04} blur={hovered ? 0:10 } radius={"md"}/>
     </AspectRatio>
-
-    // </div>
-    
   );
 }
 
