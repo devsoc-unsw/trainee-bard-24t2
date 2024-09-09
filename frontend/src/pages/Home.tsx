b899005a1de69cd84ba904b81457993dca49178d
Tet Nay Lin <tetnaylin168@gmail.com>
2024-09-10 02:22:49 +1000

    Add Navbar component to homepage

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,10 +3,12 @@ import { LeadGrid } from "../components/LeadGrid";
 import '../App.css';
 import logo from "../assets/logo_fruit_o.png";
 import './Home.css';
+import Navbar from "../components/Navbar";
 
 function Home() {
   return (
     <div className="background-gradient">
+      <Navbar/>
       <div>
         <img src={logo} alt="Logo" className="home-logo" />
       </div>
