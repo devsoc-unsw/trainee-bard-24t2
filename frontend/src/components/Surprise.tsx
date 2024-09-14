dd6765bb1ec3285b70e8f663e9ea7794eccc2668
Isaac Kim <isaackim1412@gmail.com>
2024-09-14 15:52:19 +1000

    surprise page completed

--- frontend/src/components/Surprise.tsx
+++ frontend/src/components/Surprise.tsx
@@ -1,16 +1,51 @@
-import React from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { Grid, Skeleton, Container } from "@mantine/core";
+import { useNavigate } from 'react-router-dom';
 import styles from "./Surprise.module.css";
+import axios from "axios";
 
-function Surprise({
-	height
-}) {
+function Surprise() {
+	const [fruitNames, setFruitNames] = useState<Array<string>>([]);
+	const [loaded, setLoaded] = useState(false);
+	const [clickWaiting, setClickWaiting] = useState(false);
+	const navigate = useNavigate();
 
+	useEffect(() => {
+		const fetchData = async () => {
+		  try {
+			const response = await axios.get(`http://localhost:5180/valid-search-terms/`);
+			const fruits: Array<string> = Array.from(new Set(
+								response.data.filter(item => item.type === "fruit")
+								.map(item => item.word)));
+			setFruitNames(fruits);
+			setLoaded(true);
+		  } catch (error) {
+			console.error('Error fetching data:', error);
+		  }
+		}
+		fetchData();
+	}, []);
+
+	const randomFruitPage = useCallback(() => {
+		if(loaded)  {
+			const randomIdx = Math.floor(Math.random() * fruitNames.length);
+			navigate(`/fruit/${fruitNames[randomIdx]}`); 
+		} else {
+			setClickWaiting(true);
+		}
+	}, [loaded]);
+
+	useEffect(() => {
+		if(loaded && clickWaiting) {
+			randomFruitPage();
+			setClickWaiting(false);
+		}
+	})
 	return (
-		<Container className={styles.GlassPanel} >
-            <h2 className={styles.SurpriseText}>Surprise Me</h2>
+		<Container className={styles.GlassPanel} onClick={randomFruitPage}>
+			<h2 className={styles.SurpriseText}>Surprise Me</h2>
 		</Container>
-	);
+);
 }
 
 export default Surprise;
